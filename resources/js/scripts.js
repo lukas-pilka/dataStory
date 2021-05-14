1b638000e37454b6aca13c32c705b8266c1eba69
Lukas Pilka <pilka@blueghost.cz>
2021-05-14 16:27:08 +0200

    Přidává iframy s datastudiem

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -6,3 +6,15 @@ function openMenu() {
     let menuTrigger = document.getElementById("menuTrigger");
     menuTrigger.classList.toggle("menuHidden");
 }
+function resizeDataStudio() {
+    let widowWidth = window.innerWidth;
+    let dsHeight = widowWidth * 0.7666;
+    let dsIframe = document.getElementsByClassName("dsIframe");
+    let i;
+    for (i = 0; i < dsIframe.length; i++) {
+        dsIframe[i].style.height = dsHeight;
+    }
+}
+
+window.onload = resizeDataStudio;
+window.onresize = resizeDataStudio;
