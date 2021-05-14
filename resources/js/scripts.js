1b9d678b7996c5a39a0efaf34357fa6b6006dc13
Lukas Pilka <pilka@blueghost.cz>
2021-05-14 14:35:25 +0200

    Přidává navigaci

---
+++ resources/js/scripts.js
@@ -0,0 +1,8 @@
+
+
+function openMenu() {
+    let menuItems = document.getElementById("menuItems");
+    menuItems.classList.toggle("menuHidden");
+    let menuTrigger = document.getElementById("menuTrigger");
+    menuTrigger.classList.toggle("menuHidden");
+}
